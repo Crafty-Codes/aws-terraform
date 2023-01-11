4f0cd535003f56ddcdf000bee2f3f241c836bb83
craftycode <fabio.oberhofer@gmx.net>
2023-01-12 00:15:09 +0100

    src: working

--- aws.tf
+++ aws.tf
@@ -30,19 +30,10 @@ resource "aws_instance" "server" {
 
 resource "aws_security_group" "server_open" {
     name        = "server_open"
-    # SSH 
     ingress {
-        from_port   = 22 
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    # OpenVpn Server
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "udp"
+        from_port   = 0 
+        to_port     = 0
+        protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
